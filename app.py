ac29c01584a18651b549a7f79213f69f01c2e5bf
vivien <you@example.com>
2021-06-02 11:50:44 +0200

    hello

--- app.py
+++ app.py
@@ -1,31 +1,17 @@
 from fastapi import FastAPI
 import pickle
-from sklearn.feature_extraction.text import TfidfVectorizer
+
+# from sklearn.feature_extraction.text import TfidfVectorizer
 
 """
-{
-        "name": "api",
-        "runtime": "python3.7",
-        "endpoint": "https://knd7yw.deta.dev",
-        "visor": "enabled",
-        "http_auth": "disabled"
-}
+heroku url : https://lit-spire-48980.herokuapp.com/
 """
 app = FastAPI()
 
 
-@app.get("/")
-def read_root():
+@app.get("/{input}")
+def predict(input: str):
     tfidf, model = pickle.load(open('model.bin', 'rb'))
-    lol = pickle.load(open('lol', 'rb'))
-    # print('model loaded!')
-    input = "i'm sad"
     predictions = model.predict(tfidf.transform([input]))
     label = predictions[0]
     return('{} : {}'.format(input, label))
-    # return {"Hello": "World"}
-
-
-@app.get("/items/{item_id}")
-def read_item(item_id: int):
-    return {"item_id": item_id}
