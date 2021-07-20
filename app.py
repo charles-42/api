f7dbe6ed9f6353d796e88c9114d88d0eb493a938
charles-42 <71760090+charles-42@users.noreply.github.com>
2021-07-20 16:17:38 +0200

    authentification

--- app.py
+++ app.py
@@ -1,15 +1,45 @@
-from fastapi import FastAPI
-import pickle
+import sys
+sys.path.insert(0, "/Users/charles/github/monitoring")
 
-'''
-heroku url : https://lit-spire-48980.herokuapp.com/
-'''
+import pickle
+import uvicorn
+
+from fastapi import Depends, FastAPI, HTTPException, status
+from fastapi.security import OAuth2PasswordBearer, OAuth2PasswordRequestForm
+from datetime import datetime, timedelta
+from models import Token, TokenData, User, UserInDB
+from utils import verify_password, get_password_hash, get_user,authenticate_user,create_access_token,get_current_user, get_current_active_user
+from database import fake_users_db
+from config import SECRET_KEY, ALGORITHM, ACCESS_TOKEN_EXPIRE_MINUTES
+
 app = FastAPI()
 
+@app.post("/token", response_model=Token)
+async def login_for_access_token(form_data: OAuth2PasswordRequestForm = Depends()):
+    user = authenticate_user(fake_users_db, form_data.username, form_data.password)
+    if not user:
+        raise HTTPException(
+            status_code=status.HTTP_401_UNAUTHORIZED,
+            detail="Incorrect username or password",
+            headers={"WWW-Authenticate": "Bearer"},
+        )
+    access_token_expires = timedelta(minutes=ACCESS_TOKEN_EXPIRE_MINUTES)
+    access_token = create_access_token(
+        data={"sub": user.username}, expires_delta=access_token_expires
+    )
+    return {"access_token": access_token, "token_type": "bearer"}
+
+@app.get("/")
+def main():
+    return "hello"
 
 @app.get("/{input}")
-def predict(input: str):
+def predict(input: str,current_user: User = Depends(get_current_active_user)):
     tfidf, model = pickle.load(open('model.bin', 'rb'))
     predictions = model.predict(tfidf.transform([input]))
     label = predictions[0]
     return {'text': input, 'label': label}
+
+
+if __name__ == "__main__":
+    uvicorn.run(app, host="127.0.0.1", port=8081)
